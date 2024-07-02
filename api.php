d504dd53e8025e79937911f5301fb4310f2a4558
covenant123456 <126474159+covenant123456@users.noreply.github.com>
2024-07-02 12:26:02 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,57 +1,60 @@
 <?php
-header('Content-Type: application/json');
+// Function to get client IP address from the client request
+function get_client_ip() {
+    $ipaddress = '';
 
-// Function to get client IP address
-function get_ip() {
-    if (getenv('HTTP_CLIENT_IP'))
-        return getenv('HTTP_CLIENT_IP');
-    else if (getenv('HTTP_X_FORWARDED_FOR'))
-        return getenv('HTTP_X_FORWARDED_FOR');
-    else if (getenv('HTTP_X_FORWARDED'))
-        return getenv('HTTP_X_FORWARDED');
-    else if (getenv('HTTP_FORWARDED_FOR'))
-        return getenv('HTTP_FORWARDED_FOR');
-    else if (getenv('HTTP_FORWARDED'))
-        return getenv('HTTP_FORWARDED');
-    else if (getenv('REMOTE_ADDR'))
-        return getenv('REMOTE_ADDR');
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
     else
-        return 'UNKNOWN';
+        $ipaddress = 'UNKNOWN';
+
+    return $ipaddress;
 }
+
+// Get the visitor's name from the query string
+$visitor_name = isset($_GET['visitor_name']) ? $_GET['visitor_name'] : 'Guest';
 
 // Get client IP address
-$clientIP = get_ip();
-if ($clientIP == '::1') {
-    $clientIP = '8.8.8.8'; // Default to Google's public DNS server for local testing
+$client_ip = get_client_ip();
+
+// Use GeoPlugin API to get location information based on IP address
+$geoPluginUrl = "http://www.geoplugin.net/json.gp?ip={$client_ip}";
+$locationData = json_decode(file_get_contents($geoPluginUrl), true);
+
+$location = isset($locationData['geoplugin_city']) ? $locationData['geoplugin_city'] : 'Unknown Location';
+
+// If the city is unknown, set a default location
+if ($location === 'Unknown Location') {
+    $location = 'New York'; // Default location
 }
 
-$visitorName = isset($_POST['visitor_name']) ? htmlspecialchars($_POST['visitor_name']) : 'Visitor';
-
-// Use ipinfo.io API to get location information
-$ipinfoToken = '8c8beab3595e6c';
-$ipinfoUrl = "https://ipinfo.io/{$clientIP}?token={$ipinfoToken}";
-$locationData = json_decode(file_get_contents($ipinfoUrl), true);
-
-$city = isset($locationData['city']) ? $locationData['city'] : 'Unknown Location';
-
-// If the city is unknown, set a default city for weather information
-if ($city === 'Unknown Location') {
-    $city = 'New York'; // Default city
-}
-
-// Use OpenWeatherMap API to get temperature information
+// Use OpenWeatherMap API to get current weather information for the location
 $weatherApiKey = 'e9c7326922d0a6298549bb56ddb8e8ee';
-$weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$weatherApiKey}&units=metric";
+$weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q={$location}&appid={$weatherApiKey}&units=metric";
 $weatherData = json_decode(file_get_contents($weatherUrl), true);
 
 $temperature = isset($weatherData['main']['temp']) ? $weatherData['main']['temp'] : 'Unknown';
-$greeting = "Hello, $visitorName!, the temperature is $temperature degrees Celsius in $city";
 
+// Create the response array
 $response = [
-    'client_ip' => $clientIP,
-    'location' => $city,
-    'greeting' => $greeting,
+    'client_ip' => $client_ip,
+    'location' => $location,
+    'greeting' => "Hello, $visitor_name! The temperature is {$temperature} degrees Celsius in {$location}"
 ];
 
+// Set the Content-Type to application/json
+header('Content-Type: application/json');
+
+// Print the JSON-encoded response
 echo json_encode($response);
 ?>
